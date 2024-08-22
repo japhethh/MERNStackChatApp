f75e18fcd63800a4c9b649263f4e3e13c40113fa
japhethh <boknoy32130@gmail.com>
2024-08-23 00:20:29 +0800

    Single Chat new message

--- frontend/src/components/SingleChat.tsx
+++ frontend/src/components/SingleChat.tsx
@@ -1,18 +1,68 @@
-import  { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ChatContext } from '../context/ChatProvider'
+import { UserContext } from '../context/UserContext'
 import { LuArrowLeft } from "react-icons/lu";
 import { genSender, genSenderFull } from '../config/ChatLogics';
 import ProfileModal from '../miscellaneous/ProfileModal'
 import UpdateGroupChatModel from '../miscellaneous/UpdateGroupChatModel';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 const SingleChat = ({ fetchAgain, setFetchAgain }: any) => {
+  const [messages, setMessages] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean>(false);
+  const [newMessage, setNewMessage] = useState("");
 
   const context = useContext(ChatContext);
   if (!context) {
     return null;
   }
 
+  const userContext = useContext(UserContext);
+  if (!userContext) {
+    return null;
+  }
+
+
   const { user, selectedChat, setSelectedChat } = context;
+
+  const { apiURL } = userContext;
+
+  const sendMessage = async (event: any) => {
+    if (event.key === "Enter" && newMessage) {
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.token}`
+          }
+        }
+
+        const { data } = await axios.post(`${apiURL}/api/message`,
+          {
+            content: newMessage,
+            chatId: selectedChat._id
+          }, config);
+
+        setNewMessage("");
+        setMessages([...messages, data])
+      console.log(messages)
+      
+      } catch (error) {
+        toast.error("Error Occured");
+      }
+    }
+  }
+
+  const typeHandler = (e: any) => {
+    setNewMessage(e.target.value);
+  }
+
+
+  useEffect(() => {
+    console.log(newMessage)
+  }, [])
 
   return (
     <>
@@ -33,12 +83,23 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: any) => {
                 </>) : (
                 <>
                   {selectedChat.chatName.toUpperCase()}
-                  <UpdateGroupChatModel fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
+                  <UpdateGroupChatModel fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
                 </>)}
             </div>
-            <div className="flex flex-col justify-end p-3 bg-[#E8E8E8] w-full h-full overflow-y-hidden">
-                  {/* Message Here */}
+            <div className="flex flex-col justify-center p-3 bg-[#E8E8E8] w-full h-full overflow-y-hidden">
+              {/* Message Here */}
+              {!loading ? (
+                <span className="loading loading-spinner loading-lg text-2xl h-20 self-center m-auto"></span>
+              ) : (
+                <h1>Not working well</h1>
+              )}
+
+              <div onKeyDown={sendMessage} className="mt-3 ">
+                <input className="input input-bordered w-full" type="text" placeholder="Enter messages" onChange={typeHandler} value={newMessage} />
+              </div>
             </div>
+
+
           </> : (
             <div className="flex justify-center items-center h-full w-full">
               <h1 className='text-3xl text-center pb-3 font-sans'>
